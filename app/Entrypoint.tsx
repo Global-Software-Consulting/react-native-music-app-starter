e51c17f0066b0d9a2149d9242fb65ca3288c7cac
Noor Un Nisa <noorunnisa@gsoftConsulting.com>
2021-09-29 17:07:46 +0500

    Orientation

--- app/Entrypoint.tsx
+++ app/Entrypoint.tsx
@@ -3,7 +3,7 @@
  * Everything starts from the Entry-point
  */
 import React, { useEffect } from 'react';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator,LogBox } from 'react-native';
 import { Provider, useSelector } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { Provider as PaperProvider } from 'react-native-paper';
@@ -21,6 +21,8 @@ interface IState {
 }
 
 const RootNavigation: React.FC = () => {
+    LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+    LogBox.ignoreAllLogs(); //Ignore all log notifications
     const isDark = useSelector((state: IState) => state.themeReducer.isDark);
     return (
         <PaperProvider theme={isDark ? darkTheme : defaultTheme}>
